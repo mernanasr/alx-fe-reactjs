8647f898a03175c90f2bc0fe82cf2b15b57ad845
merna estafanous <mernanasr55@gmail.com>
2025-02-23 23:57:49 +0200

    Applied inline css styling to components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px'}}>
+        <p>© 2024 City Lovers</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+    return (
+      <main style={{ padding: '20px', backgroundColor: '#f4f4f4'}}>
+        <p style={{ fontSize: '18px'}}>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+  };
+  
+  export default MainContent;
+  
