3d7b2f021a5910e38ba3f1cfc963a0d7f6433223
merna estafanous <mernanasr55@gmail.com>
2025-03-02 20:30:09 +0200

    Initial commit - company website

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ marginTop: '20px', padding: '10px', backgroundColor: '#222', color: 'white', textAlign: 'center' }}>
+        <p>&copy; 2025 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
