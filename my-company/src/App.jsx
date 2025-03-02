cfc851136c1a0093ab32547c727449f02a2371df
merna estafanous <mernanasr55@gmail.com>
2025-03-02 20:30:09 +0200

    Initial commit - company website

---
+++ my-company/src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
