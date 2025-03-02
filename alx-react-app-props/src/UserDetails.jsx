fc0ec465ba6c7ac36c4dc56e3815a84c1e7f0fdf
merna estafanous <mernanasr55@gmail.com>
2025-03-02 20:11:06 +0200

    Reimplemented UserContext.js and related files

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,5 +1,6 @@
-import { useContext } from "react";
-import UserContext from "../UserContext";
+
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
