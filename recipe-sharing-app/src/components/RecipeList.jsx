f4e772c41a2897899e8ef66e0ac9be1043014e2f
merna estafanous <mernanasr55@gmail.com>
2025-03-02 23:09:14 +0200

    Enhanced Recipe Sharing App with detailed recipe management

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
-import useRecipeStore from '../store/recipeStore';
+import { Link } from 'react-router-dom';
+import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -11,8 +12,9 @@ const RecipeList = () => {
       ) : (
         recipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
           </div>
         ))
       )}
