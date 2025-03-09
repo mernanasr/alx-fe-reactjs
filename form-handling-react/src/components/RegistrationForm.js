46105df6de7d0844023b98fc43e01dda65f9a619
merna estafanous <mernanasr55@gmail.com>
2025-03-09 18:28:06 +0200

    fixed some files

--- form-handling-react/src/components/RegistrationForm.js
+++ form-handling-react/src/components/RegistrationForm.js
@@ -36,21 +36,39 @@ const RegistrationForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" name="username" value={formData.username} onChange={handleChange} />
-        {errors.username && <p>{errors.username}</p>}
+        <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        {errors.username && <span style={{ color: "red" }}>{errors.username}</span>}
       </div>
 
       <div>
-        <label>Email:</label>
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
-        {errors.email && <p>{errors.email}</p>}
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
       </div>
 
       <div>
-        <label>Password:</label>
-        <input type="password" name="password" value={formData.password} onChange={handleChange} />
-        {errors.password && <p>{errors.password}</p>}
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        {errors.password && <span style={{ color: "red" }}>{errors.password}</span>}
       </div>
 
       <button type="submit">Register</button>
@@ -59,3 +77,4 @@ const RegistrationForm = () => {
 };
 
 export default RegistrationForm;
+
