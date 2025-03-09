5179fd7f2b6ce6669dfbbbebe13bb17984327464
merna estafanous <mernanasr55@gmail.com>
2025-03-09 20:07:48 +0200

    updated files

--- form-handling-react/vite.config.js
+++ form-handling-react/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+import react from "@vitejs/plugin-react";
+
+export default {
   plugins: [react()],
-})
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+    loader: "jsx", // Ensures JSX works inside .js files
+  },
+};
+
+
