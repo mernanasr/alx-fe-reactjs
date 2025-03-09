7f93460205e6c10ebb0e77c9f49b0e1ac1d8e0cd
merna estafanous <mernanasr55@gmail.com>
2025-03-09 20:07:48 +0200

    updated files

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm.jsx";
+import FormikForm from "./components/formikForm.js";
 import "./App.css";
 
 function App() {
