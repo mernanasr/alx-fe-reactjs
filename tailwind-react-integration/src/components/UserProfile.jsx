6b068472e3284dd50008f97eed291567505c210d
merna estafanous <mernanasr55@gmail.com>
2025-03-16 03:15:00 +0200

    updated lines

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,12 +4,12 @@ function UserProfile() {
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
+          className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
         />
         <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
           John Doe
         </h1>
-        <p className="text-sm sm:text-base text-gray-600">
+        <p className="text-sm sm:text-base md:text-xl text-gray-600">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
