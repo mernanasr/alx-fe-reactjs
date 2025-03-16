12f4f71add423b27644a577b8b1c6696420d24dc
merna estafanous <mernanasr55@gmail.com>
2025-03-16 03:05:01 +0200

    modifying UserProfile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img
         className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
         src="https://via.placeholder.com/150"
