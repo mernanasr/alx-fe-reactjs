f2fd9cb12dd5b4709d5cf75127ae7c277434c4a2
merna estafanous <mernanasr55@gmail.com>
2025-03-16 03:08:13 +0200

    modifying file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,20 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
         <img
-        className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
-        src="https://via.placeholder.com/150"
-         alt="User"
-          />
-        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-sm sm:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
+          src="https://via.placeholder.com/150"
+          alt="User"
+          className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
+        />
+        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
+          John Doe
+        </h1>
+        <p className="text-sm sm:text-base text-gray-600">
+          Developer at Example Co. Loves to write code and explore new technologies.
+        </p>
       </div>
     );
   }
   
   export default UserProfile;
+  
