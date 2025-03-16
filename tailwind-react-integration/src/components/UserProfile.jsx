de6b191cde5339a4d26b855bf8649c747a759be0
merna estafanous <mernanasr55@gmail.com>
2025-03-16 03:22:41 +0200

    added hovers to the card parts

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center transition-shadow duration-300 ease-in-out hover:shadow-xl">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
+          className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
         />
-        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">
+        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 transition-shadow duration-300 ease-in-out hover:text-blue-500">
           John Doe
         </h1>
         <p className="text-sm sm:text-base md:text-xl text-gray-600">
